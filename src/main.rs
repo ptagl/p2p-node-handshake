1a9ce9c43e543b0e3ee88ec07f531f7dd78bfc0d
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2023-12-16 11:02:46 +0100

    Read TCP stream to check connection status
    Currently the connection times out in 15 seconds.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::net::TcpStream;
+use std::{
+    io::{ErrorKind, Read},
+    net::TcpStream,
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
 fn main() {
     println!("Hello, world!");
@@ -7,8 +12,46 @@ fn main() {
 }
 
 fn connect_to_node(destination_address: &str) {
-    match TcpStream::connect(destination_address) {
-        Ok(_) => println!("Connected succesfully"),
-        Err(error) => println!("Connection error: {:?}", error),
+    let mut stream = match TcpStream::connect(destination_address) {
+        Ok(stream) => stream,
+        Err(error) => {
+            println!("Connection error: {:?}", error);
+            return;
+        }
+    };
+
+    let start = Instant::now();
+
+    println!("Connected succesfully");
+
+    // Set the stream as non-blocking when performing read/write operations
+    if let Err(error) = stream.set_nonblocking(true) {
+        println!(
+            "Error while setting the socket as non-blocking: {:?}",
+            error
+        );
     }
+
+    let mut buffer = [0u8; 1024];
+
+    loop {
+        sleep(Duration::from_millis(100));
+
+        match stream.read(&mut buffer) {
+            // EOF, connection closed
+            Ok(0) => break,
+            Ok(read_bytes) => println!("Read {} bytes from the socket", read_bytes),
+            // WouldBlock is returned by non-blocking streams when there is no data to read yet
+            Err(error) if error.kind() == ErrorKind::WouldBlock => {}
+            Err(error) => {
+                println!("Unexpected stream error: {:?}", error);
+                break;
+            }
+        }
+    }
+
+    println!(
+        "Connection closed after {} seconds",
+        start.elapsed().as_secs()
+    );
 }
