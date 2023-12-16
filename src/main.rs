bf7008cec0201cf95539a594fff5e593b076a8f4
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2023-12-16 10:19:17 +0100

    TCP connection established

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpStream;
+
 fn main() {
     println!("Hello, world!");
+
+    connect_to_node("127.0.0.1:9651")
+}
+
+fn connect_to_node(destination_address: &str) {
+    match TcpStream::connect(destination_address) {
+        Ok(_) => println!("Connected succesfully"),
+        Err(error) => println!("Connection error: {:?}", error),
+    }
 }
